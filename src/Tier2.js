9220e3cb2261a1b9a2b7182c36b9d6f835507949
derrick b <dbozich1@gmail.com>
2018-10-26 06:33:57 +0000

    automatically backed up by learn

--- src/Tier2.js
+++ src/Tier2.js
@@ -35,7 +35,7 @@ export default class Tier2 extends Component {
     // present in our solution. What should they be replaced with?
     return (
       <div onClick={console.log('2')} className="tier2" style={{backgroundColor: this.props.color, color: this.props.color}}>
-        <Tier3 color={"#0F0"} />
+        <Tier3 color={this.state.childColor} />
         <Tier3 color={"#F00"} />
       </div>
     )
