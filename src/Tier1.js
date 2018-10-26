40ed8b060cdfa9a080d35c662a50c9f159b3b104
derrick b <dbozich1@gmail.com>
2018-10-26 07:15:05 +0000

    automatically backed up by learn

--- src/Tier1.js
+++ src/Tier1.js
@@ -35,7 +35,7 @@ export default class Tier1 extends Component {
   }
 
   handleChildClick = (e) => {
-    
+
   }
 
   handleGrandChildClick = (e) => {
@@ -47,7 +47,7 @@ export default class Tier1 extends Component {
     // present in our solution. What should they be replaced with?
     return (
       <div onClick={() => this.handleClick()} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
-        <Tier2 color={this.state.childColor} childColor={this.state.grandChildColor} handleChildClick={e => this.handleChildClick} handleGrandChildClick={e => this.handleGrandChildClick} />
+        <Tier2 color={this.state.childColor} childColor={this.state.grandChildColor} handleClick={e => this.handleChildClick} handleChildClick={e => this.handleGrandChildClick} />
         <Tier2 color={getReducedColor(this.state.color)} />
       </div>
     )
