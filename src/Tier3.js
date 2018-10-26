b528a6b4950b989f4aea5ab380965626180ccbaf
derrick b <dbozich1@gmail.com>
2018-10-26 05:48:00 +0000

    automatically backed up by learn

--- src/Tier3.js
+++ src/Tier3.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { getRandomColor } from './randomColorGenerator.js'
 
-let color = props.color
+
 const Tier3 = (props) => (
-  
+
 
   // this component does not need to change (but feel free to change however you like!)
-  <div className="tier3" onClick={color = getRandomColor()} style={{backgroundColor: color, color: color}}></div>
+  <div className="tier3" onClick={console.log('3')} style={{backgroundColor: color, color: color}}></div>
 )
 
 export default Tier3
