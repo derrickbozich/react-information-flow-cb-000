9cfa30e18db6e6bba683032f3ce06d31aed49c8f
derrick b <dbozich1@gmail.com>
2018-10-26 05:52:59 +0000

    automatically backed up by learn

--- src/Tier2.js
+++ src/Tier2.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import { getRandomColor, getReducedColor } from './randomColorGenerator.js'
+import { getReducedColor } from './randomColorGenerator.js'
 import Tier3 from './Tier3'
-
 
 
 export default class Tier2 extends Component {
@@ -13,32 +12,13 @@ export default class Tier2 extends Component {
     }
   }
 
-  componentWillReceiveProps(nextProps){
-    this.setState({
-      color: nextProps.color,
-      childColor: getReducedColor(nextProps.color)
-    })
-  }
-
-  handleClick = (e) => {
-    e.stopPropagation()
-    let newColor = getRandomColor()
-    this.setState({
-      color: newColor,
-      childColor: getReducedColor(newColor)
-    })
-  }
-
   render() {
     // hard coded color values have been added below, though they won't be
     // present in our solution. What should they be replaced with?
-
-    let p = this.props
-
     return (
-      <div onClick={this.handleClick } className="tier2" style={{backgroundColor: this.state.color, color: this.state.color}}>
-        <Tier3 color={this.state.childColor} handleChildClick={e => this.handleChildClick(e)} />
-        <Tier3 color={this.state.childColor} handleChildClick={e => this.handleChildClick(e)} />
+      <div className="tier2" style={{backgroundColor: this.props.color, color: this.props.color}}>
+        <Tier3 color={"#0F0"} />
+        <Tier3 color={"#F00"} />
       </div>
     )
   }
