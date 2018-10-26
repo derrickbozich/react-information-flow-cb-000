ae3d166396f83236343e49d1f783d9442c7adf53
derrick b <dbozich1@gmail.com>
2018-10-26 07:10:59 +0000

    automatically backed up by learn

--- src/Tier1.js
+++ src/Tier1.js
@@ -17,26 +17,37 @@ export default class Tier1 extends Component {
 
   generateColors = (count=3) => {
     const colors = [getRandomColor()]
-    debugger
+
     for (let idx = 1; idx < count; idx++) {
       colors.push(getReducedColor(colors[colors.length - 1]))
     }
     return colors
   }
 
-  updateColor = () => {
-    let newColor = getRandomColor()
+  handleClick = () => {
+    const [c1, c2, c3] = this.generateColors()
     this.setState({
-      color: newColor
+      color: c1,
+      childColor: c2,
+      grandChildColor: c3
     })
+
+  }
+
+  handleChildClick = () => {
+
+  }
+
+  handleGrandChildClick = () => {
+
   }
 
   render() {
     // hard coded color values have been added below, though they won't be
     // present in our solution. What should they be replaced with?
     return (
-      <div onClick={() => this.updateColor()} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
-        <Tier2 color={getReducedColor(this.state.color)} />
+      <div onClick={() => this.handleClick()} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
+        <Tier2 color={} />
         <Tier2 color={getReducedColor(this.state.color)} />
       </div>
     )
